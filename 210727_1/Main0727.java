cd9069f7327a9a3ad337c5e587d7f2db72278d6f
GB <nicehe74@gmail.com>
2021-07-27 21:32:31 +0900

    제곱근판별

---
+++ 210727_1/Main0727.java
@@ -0,0 +1,12 @@
+public class Main0727 {
+    public static void main(String[] args){
+
+            double n = Math.sqrt(122);
+            if(Math.floor(n)==n){
+                System.out.println(Math.pow(n+1, 2));
+            }else{
+                System.out.println(-1);
+            }
+
+    }
+}
