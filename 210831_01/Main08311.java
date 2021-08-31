d0970884a311e75de5e776149b265624a0fb40cb
HaeunJeong <nicehe74@gmail.com>
2021-08-31 22:38:13 +0900

    0831 -1

---
+++ 210831_01/Main08311.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main08311 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        return map.size() < nums.length/2 ? map.size() : nums.length/2;
+    }
+}
