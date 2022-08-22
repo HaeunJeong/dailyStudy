b089af80290e904f13026c274c3c69f1ad150889
HaeunJeong <nicehe74@gmail.com>
2022-08-22 10:52:59 +0900

    3xn타일링, n&2배열자르기

---
+++ 22년도/220822/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+    public int solution(int n) {
+        int answer = 0;
+        long[] dp = new long[5001];
+
+        dp[0] = 1;
+        dp[2] = 3;
+        for(int i=4; i<=n; i+=2){
+            dp[i] = dp[i-2] * 3;
+            for(int j=i-4; j>=0; j-=2){
+                dp[i] += dp[j] * 2;
+            }
+            dp[i] = dp[i] % 1000000007;
+        }
+
+        answer = (int) dp[n];
+        return answer;
+    }
+}
