bec728bece792608109b0636e077acbe29a3f494
HaeunJeong <nicehe74@gmail.com>
2022-12-12 11:45:06 +0900

    숫자찾기

--- 22년도/221212/Solution2.java
+++ 22년도/221212/Solution2.java
@@ -1,2 +1,14 @@
 public class Solution2 {
+
+    public int solution(int num, int k) {
+
+        char[] chars = Integer.toString(num).toCharArray();
+        for(int i = 0; i<chars.length; i++){
+            if(k == Integer.parseInt(chars[i]+"")){
+                return i+1;
+            }
+        }
+        return -1;
+
+    }
 }
