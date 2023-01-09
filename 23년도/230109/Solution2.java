c3830396c9b232b625e3b495d599aeb7ddbc2da1
HaeunJeong <nicehe74@gmail.com>
2023-01-09 12:15:35 +0900

    아이스아메리카노

--- 23년도/230109/Solution2.java
+++ 23년도/230109/Solution2.java
@@ -1,2 +1,8 @@
 public class Solution2 {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money/5500;
+        answer[1] = (int) money%5500;
+        return answer;
+    }
 }
